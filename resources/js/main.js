164e5bfe8b7bc1694c6b2a381cb0fa3dc0272379
egbusch <egbusch@uc.cl>
2017-12-31 10:40:23 -0300

    THE END

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
     dots: true,
     infinite: true
   });
+
+  var $scrollx = 0;
+  $(window).scroll(function() {
+    var $scrolly = $(this).scrollTop();
+    if ($scrolly - $scrollx > 50) {
+      var $scrollz = $('nav').css('height');
+      $('nav').animate({top: '-' + $scrollz}, 150);
+      $scrollx = $scrolly;
+    } else if ($scrollx - $scrolly > 50) {
+      $('nav').animate({top: '0px'}, 150);
+      $scrollx = $scrolly;
+    }
+    // console.log($scrollx, $scrolly, $scrollz);
+  });
 });
