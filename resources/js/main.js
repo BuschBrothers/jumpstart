339bb8033faff81df0c62a0c13bcd79fc78f04dd
egbusch <egbusch@uc.cl>
2017-12-30 21:52:29 -0300

    almost there

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+  $(".carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    swipe: false,
+    dots: true,
+    infinite: true
+  });
+});
